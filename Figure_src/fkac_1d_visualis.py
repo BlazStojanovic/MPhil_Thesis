4d61d7385d1833642cdeeebd032fa4dc1cc391ee
Blaž Stojanovič <bs673@cam.ac.uk>
2021-05-18 11:09:03 +0100

    some writing of the introduction on feynman kac

---
+++ Figure_src/fkac_1d_visualis.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Brownian motion
+def bmotion():
+	pass
+
+# Analytical solution of Fokker-Plant
+def f_plank_1d():
+	pass
+
+# Fkac average 
+def f_kac():
+	pass

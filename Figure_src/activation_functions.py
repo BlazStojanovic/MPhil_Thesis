5a0cbf2cebf427428f7afc0bec4453d55fa69fb9
csc2006 <csc2006@csc20-laptop-06>
2021-07-22 22:27:09 +0100

    intros to chapters 2, 4, 5, added three figures for ML chapter

---
+++ Figure_src/activation_functions.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib
+from matplotlib import rc
+matplotlib.rcParams.update({'font.size': 22})
+matplotlib.rcParams['text.usetex'] = True
+matplotlib.rcParams['text.latex.preamble'] = r'\usepackage{libertine}'
+
+def ReLu(x):
+	if x > 0:
+		return x
+	else:
+		return 0
+
+def Softplus(x):
+	return np.log(1 + np.exp(x))
+
+x = np.linspace(-3, 3, 1000)
+y1 = [ReLu(a) for a in x]
+y2 = Softplus(x)
+
+fig = plt.figure(figsize=(12, 7))
+plt.plot(x, y1, label='ReLu', color='blue', linewidth=2)
+plt.plot(x, y2, label='Softplus', color='red', linewidth=2)
+
+plt.xlabel("$x$")
+plt.ylabel(r"$\sigma (x)$")
+plt.legend()
+plt.savefig("../Chapter4/Figs/Vector/activations.pdf", bbox_inches='tight')
+# plt.show()
+
